857a9f5fd53fec4a14e49627ef9628b6f72f7450
Christian Petersen <humanoidism@gmail.com>
2016-01-05 10:14:06 +0100

    Add completions for lunchy

--- topic/completion.zsh
+++ topic/completion.zsh
@@ -1,1 +1,7 @@
 eval "`npm completion`"
+
+# completion for lunchy
+LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
+if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
+  . $LUNCHY_DIR/lunchy-completion.zsh
+fi
